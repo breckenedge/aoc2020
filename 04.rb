fca4fb1f45c10a3fe2b9a574f375260e21cb5cec
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-12-04 09:03:29 -0600

    Day 4 Part 2

--- 04.rb
+++ 04.rb
@@ -1,9 +1,30 @@
 REQUIRED_FIELDS = %w[byr iyr eyr hgt hcl ecl pid cid]
 EXCEPT = %w[cid]
+EYE_COLORS = %w[amb blu brn gry grn hzl oth]
 
 def passport_valid?(passport)
   (REQUIRED_FIELDS - EXCEPT).all? do |field|
     passport.key?(field)
+  end
+end
+
+def passport_data_valid?(passport)
+  ('1920'..'2002').include?(passport['byr']) &&
+    ('2010'..'2020').include?(passport['iyr']) &&
+    ('2020'..'2030').include?(passport['eyr']) &&
+    height_valid?(passport['hgt']) &&
+    /\A\#[0-9a-f]{6}\z/.match?(passport['hcl']) &&
+    EYE_COLORS.include?(passport['ecl']) &&
+    /\A\d{9}\z/.match?(passport['pid'])
+end
+
+def height_valid?(height)
+  if height =~ /in\z/
+    (59..76).include?(height.to_i)
+  elsif height =~ /cm\z/
+    (150..193).include?(height.to_i)
+  else
+    false
   end
 end
 
@@ -25,9 +46,8 @@ def p1_input
 
     entries = line.split(' ')
     entries.each do |entry|
-      entry.split(':').each do |key, value|
-        current_passport[key] = value
-      end
+      key, value = entry.split(':')
+      current_passport[key] = value
     end
   end
 
@@ -35,6 +55,9 @@ def p1_input
 end
 
 def p2(input = p2_input)
+  input.select { |passport| passport_valid?(passport) }
+    .select { |passport| passport_data_valid?(passport) }
+    .count
 end
 
 def p2_input
