cbd9ee6ca9eb0e91b367e38d3523a78e80e59fd6
Aaron Breckenridge <aaron@breckenridge.dev>
2020-12-05 09:20:05 -0600

    Day 5 Part 1

---
+++ 05.rb
@@ -0,0 +1,59 @@
+def p1(addresses = p1_input)
+  addresses.map do |addr|
+    row_low = 0
+    row_high = 127
+
+    addr[:row].each_char do |inst|
+      size = row_high - row_low
+
+      if inst == 'F'
+        row_high = row_high - (size / 2) - 1
+      else
+        row_low = row_low + (size / 2) + 1
+      end
+    end
+
+    col_low = 0
+    col_high = 7
+
+    addr[:col].each_char do |inst|
+      size = col_high - col_low
+
+      if inst == 'L'
+        col_high = col_high - (size / 2) - 1
+      else
+        col_low = col_low + (size / 2) + 1
+      end
+    end
+
+    row_low * 8 + col_low
+  end.max
+end
+
+def p1_input
+  data.map do |address|
+    {
+      row: address[0,7],
+      col: address[7, 3],
+    }
+  end
+end
+
+def p2(addresses = p2_input)
+end
+
+def p2_input
+  p1_input
+end
+
+def data(filename = '05.input')
+  @data ||= File.readlines(filename).map(&:strip)
+end
+
+def main
+  puts "part 1: #{p1}"
+  puts "part 2: #{p2}"
+  exit 0
+end
+
+main if __FILE__ == $0
