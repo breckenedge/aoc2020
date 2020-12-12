18fb4b6129d99261507e70c4c806e5fb7614311f
Aaron Breckenridge <aaron@breckenridge.dev>
2020-12-11 21:23:24 -0600

    Fix test on AdjacentGenerator; add CardinalGenerator

--- cardinal_generator.rb
+++ cardinal_generator.rb
@@ -1,5 +1,5 @@
-# Given a grid and row column indexes, get all adjacent grid entries
-module AdjacentGenerator
+# Given a grid and row column indexes, get all cardinal grid entries
+module CardinalGenerator
   Cell = Struct.new(:row, :col, :val) do
     def to_i
       val
@@ -8,23 +8,23 @@ module AdjacentGenerator
 
   module_function
 
-  ADJACENTS = [
-    [-1, -1], [-1,  0], [-1,  1],
+  CARDINALS = [
+              [-1,  0],
     [ 0, -1],           [ 0,  1],
-    [ 1, -1], [ 1,  0], [ 1,  1],
+              [ 1,  0],
   ]
 
   def call(grid, row_index, col_index)
     rows = grid.size
     cols = grid[0].size
 
-    [].tap do |adjacents|
-      ADJACENTS.each do |row_offset, col_offset|
+    [].tap do |cardinals|
+      CARDINALS.each do |row_offset, col_offset|
         curr_row = row_index + row_offset
         curr_col = col_index + col_offset
 
         if curr_row >= 0 && curr_col >= 0 && curr_row < rows && curr_col < cols
-          adjacents << Cell.new(curr_row, curr_col, grid[curr_row][curr_col])
+          cardinals << Cell.new(curr_row, curr_col, grid[curr_row][curr_col])
         end
       end
     end
@@ -34,7 +34,7 @@ end
 if __FILE__ == $0
   require 'test/unit'
 
-  class AdjacentGeneratorTest < Test::Unit::TestCase
+  class CardinalGeneratorTest < Test::Unit::TestCase
     def setup
       @grid = [
         [0, 1, 2],
@@ -44,15 +44,15 @@ if __FILE__ == $0
     end
 
     def test_adjacent_to_zero
-      assert_equal [1, 3, 4], AdjacentGenerator.(@grid, 0, 0).map(&:to_i)
+      assert_equal [1, 3], CardinalGenerator.(@grid, 0, 0).map(&:val)
     end
 
     def test_adjacent_to_four
-      assert_equal [0, 1, 2, 3, 5, 6, 7, 8], AdjacentGenerator.(@grid, 1, 1).map(&:to_i)
+      assert_equal [1, 3, 5, 7], CardinalGenerator.(@grid, 1, 1).map(&:val)
     end
 
     def test_adjacent_to_seven
-      assert_equal [3, 4, 5, 6, 8], AdjacentGenerator.(@grid, 2, 1).map(&:to_i)
+      assert_equal [4, 6, 8], CardinalGenerator.(@grid, 2, 1).map(&:val)
     end
   end
 end
