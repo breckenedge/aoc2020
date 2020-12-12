279be08395ae6fc49e5f57da67c6dcde2b3ece29
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-12-12 10:00:42 -0600

    Day 12 part 2

--- 12.rb
+++ 12.rb
@@ -43,17 +43,66 @@ module Day12
     data.map { |line| /(?<dir>\D+)(?<dis>\d+)/.match(line).named_captures }
   end
 
+  # Rightward rotation: (x, y) => (y, -x)
+
+  # Leftward rotation: (x, y) => (-y, x)
+
   def p2(input = p2_input)
+    ferry = { 'row' => 0, 'col' => 0 }
+    waypoint = [-1, -10]
+
+    input.each do |inst|
+      case inst['dir']
+      when 'N'
+        waypoint[0] -= inst['dis'].to_i
+      when 'S'
+        waypoint[0] += inst['dis'].to_i
+      when 'E'
+        waypoint[1] -= inst['dis'].to_i
+      when 'W'
+        waypoint[1] += inst['dis'].to_i
+      when 'L', 'R'
+        offset = inst['dis'].to_i / 90
+        dir = inst['dir']
+
+        offset.times do
+          row = waypoint[0]
+          col = waypoint[1]
+
+          if dir == 'R'
+            col *= -1
+          else
+            row *= -1
+          end
+
+          waypoint[0] = col
+          waypoint[1] = row
+        end
+      when 'F'
+        ferry['row'] += waypoint[0] * inst['dis'].to_i
+        ferry['col'] += waypoint[1] * inst['dis'].to_i
+      end
+    end
+
+    ferry['row'].abs + ferry['col'].abs
   end
 
   def p2_input
     p1_input
   end
 
-  def e1(input = e1_input)
+  def e2(input = e2_input)
+    p2(p1_input(input))
   end
 
-  def e1_input
+  def e2_input
+    <<~EOS.split("\n")
+      F10
+      N3
+      F7
+      R90
+      F11
+    EOS
   end
 
   def data(filename = '12.input')
@@ -61,8 +110,8 @@ module Day12
   end
 
   def main
-    puts "example 1: #{e1}"
     puts "part 1: #{p1}"
+    puts "example 2: #{e2}"
     puts "part 2: #{p2}"
     exit 0
   end
