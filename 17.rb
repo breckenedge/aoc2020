a671371683d8ec501915bad6e62407d4f341e297
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2020-12-17 12:43:06 -0600

    Day 17 part 2

--- 17.rb
+++ 17.rb
@@ -1,49 +1,58 @@
 module Day17
   module_function
 
-  ADJACENTS = -1.upto(1).flat_map do |x|
-    -1.upto(1).flat_map do |y|
-      -1.upto(1).map do |z|
-        [x, y, z] unless x == 0 && y == 0 && z == 0
-      end
-    end
-  end.compact
+  def adjacents(dimensions)
+    @adjacents ||= {}
+    @adjacents[dimensions] ||= if dimensions == 3
+                                 -1.upto(1).flat_map do |x|
+                                   -1.upto(1).flat_map do |y|
+                                     -1.upto(1).map do |z|
+                                       [x, y, z] unless x == 0 && y == 0 && z == 0
+                                     end
+                                   end
+                                 end.compact
+                               elsif dimensions == 4
+                                 -1.upto(1).flat_map do |x|
+                                   -1.upto(1).flat_map do |y|
+                                     -1.upto(1).flat_map do |z|
+                                       -1.upto(1).map do |w|
+                                         [x, y, z, w] unless x == 0 && y == 0 && z == 0 && w == 0
+                                       end
+                                     end
+                                   end
+                                 end.compact
+                               end
+  end
 
-  def neighbor_addresses(address)
-    ADJACENTS.map do |adj|
-      [address[0] + adj[0], address[1] + adj[1], address[2] + adj[2]]
+  def neighbor_addresses(address, dimensions)
+    adjacents(dimensions).map do |adj|
+      dimensions.times.map do |i|
+        address[i] + adj[i]
+      end
     end
   end
 
-  def p1(grid = p1_input)
+  def p1(grid = p1_input, dimensions = 3)
     new_grid = grid.clone
-
-    6.times do
-      new_grid = p1_step(new_grid)
-    end
-
+    6.times { new_grid = p1_step(new_grid, dimensions) }
     new_grid.values.count { |value| value == '#' }
   end
 
-  def p1_step(grid)
-    new_grid = Hash.new('.') # { |hash, unknown_key| hash[unknown_key] = '.' }
+  def p1_step(grid, dimensions)
+    new_grid = Hash.new
 
     grid.each do |address, _value|
-      neighbor_addresses(address).each do |naddr|
+      neighbor_addresses(address, dimensions).each do |naddr|
         value = grid[naddr]
-        active_neighbors = neighbor_addresses(naddr).select { |nnaddr| grid[nnaddr] == '#' }
+        active_neighbors = neighbor_addresses(naddr, dimensions).count { |nnaddr| grid[nnaddr] == '#' }
 
         if value == '#'
-          if (active_neighbors.count == 2 || active_neighbors.count == 3)
+          if active_neighbors == 2 || active_neighbors == 3
             new_grid[naddr] = '#'
-          else
-            new_grid[naddr] = '.'
           end
-        elsif value == '.'
-          if active_neighbors.count == 3
+        else
+          if active_neighbors == 3
             new_grid[naddr] = '#'
-          else
-            new_grid[naddr] = '.'
           end
         end
       end
@@ -52,12 +61,14 @@ module Day17
     new_grid
   end
 
-  def p1_input(data = self.data)
-    grid = Hash.new('.')
+  def p1_input(data = self.data, dimensions = 3)
+    grid = Hash.new
 
     data.each_with_index do |row, row_index|
       row.chars.each_with_index do |cell, col_index|
-        grid[[row_index, col_index, 0]] = cell
+        addr = [row_index, col_index]
+        (dimensions - 2).times { addr << 0 }
+        grid[addr] = cell
       end
     end
 
@@ -65,10 +76,11 @@ module Day17
   end
 
   def p2(input = p2_input)
+    p1(input, 4)
   end
 
   def p2_input
-    p1_input
+    p1_input(self.data, 4)
   end
 
   def e1(input = e1_input)
